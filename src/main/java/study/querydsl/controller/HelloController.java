2d8377cc077a14b8462b0b9491920f91eefc39ad
yoo-jaein <y_jaein@naver.com>
2021-06-05 14:53:22 +0900

    조회 API 컨트롤러 개발

--- src/main/java/study/querydsl/controller/HelloController.java
+++ src/main/java/study/querydsl/controller/HelloController.java
@@ -1,4 +1,4 @@
-package study.querydsl;
+package study.querydsl.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
